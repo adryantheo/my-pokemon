c898b0334f6fa55094479ec99f9de28801f092ab
A.Theo <adryantheo@gmail.com>
2021-03-28 18:53:21 +0700

    init commit

---
+++ src/Components/Root.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Root.css'
+
+export default function Root() {
+    return (
+        <div className="header">
+            <h1 className="title">Pokemon World</h1>
+        </div>
+    )
+}
